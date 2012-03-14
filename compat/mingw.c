352339e6058366881003c220b837a027bfcda0bd
Karsten Blees <blees@dcon.de>
2012-03-14 01:08:40 +0100

    Win32: fix detection of empty directories in is_dir_empty
    On Windows XP (not Win7), directories cannot be deleted while a find handle
    is open, causing "Deletion of directory '...' failed. Should I try again?"
    prompts.
    
    Prior to 19d1e75d "Win32: Unicode file name support (except dirent)",
    these failures were silently ignored due to strbuf_free in is_dir_empty
    resetting GetLastError to ERROR_SUCCESS.
    
    Close the find handle in is_dir_empty so that git doesn't block deletion
    of the directory even after all other applications have released it.
    
    Additionally, FindFirstFile fails with ERROR_FILE_NOT_FOUND instead of
    ERROR_NO_MORE_FILES if the directory is empty or doesn't exist.
    
    Reported-by: John Chen <john0312@gmail.com>
    Signed-off-by: Karsten Blees <blees@dcon.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -239,12 +239,15 @@ static int is_dir_empty(const wchar_t *wpath)
 	wcscat(wbuf, L"\\*");
 	handle = FindFirstFileW(wbuf, &findbuf);
 	if (handle == INVALID_HANDLE_VALUE)
-		return GetLastError() == ERROR_NO_MORE_FILES;
+		return GetLastError() == ERROR_FILE_NOT_FOUND;
 
 	while (!wcscmp(findbuf.cFileName, L".") ||
 			!wcscmp(findbuf.cFileName, L".."))
-		if (!FindNextFileW(handle, &findbuf))
-			return GetLastError() == ERROR_NO_MORE_FILES;
+		if (!FindNextFileW(handle, &findbuf)) {
+			DWORD err = GetLastError();
+			FindClose(handle);
+			return err == ERROR_NO_MORE_FILES;
+		}
 	FindClose(handle);
 	return 0;
 }
